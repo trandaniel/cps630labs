f5b1dd2605f2eed2cbd99aa9da2fc25dac4150f4
Daniel Tran <tran.daniel95@gmail.com>
2017-01-30 11:56:16 -0500

    Adding navbar to landing page

--- index.php
+++ index.php
@@ -3,8 +3,27 @@
   <head>
     <meta charset="utf-8">
     <title>CPS630 Labs | Daniel Tran</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style/style.css">
+    <script src="https://use.fontawesome.com/9655b265f7.js"></script>
   </head>
   <body>
-    <a href="lab1/index.html">Lab1</a>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">
+            <i class="fa fa-home" aria-hidden="true"></i>
+          </a>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="lab1/index.html">Lab 1</a></li>
+            <li><a href="#">Lab 2</a></li>
+            <li><a href="#">Lab 3</a></li>
+            <li><a href="#">Lab 4</a></li>
+            <li><a href="#">Lab 5</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </body>
 </html>
